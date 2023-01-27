3bb83e3188d2e22e3052eac690e81fb7aeeeb9e1
washington gama <wgamaneto16@gmail.com>
2023-01-27 15:00:30 -0300

    req1

---
+++ src/migrations/20230127145229-create-users.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      display_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+      }
+    },
+      {
+        indexes: [
+          {
+            unique: true,
+            fields: [ 'id', 'email' ]
+          }
+        ]
+      });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+
+  }
+};
